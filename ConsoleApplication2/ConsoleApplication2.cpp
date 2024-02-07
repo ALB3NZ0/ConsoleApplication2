3a60a16e1db84c8ef00820cf13b82d1397a46323
Rustam <144844263+ALB3NZ0@users.noreply.github.com>
2024-02-07 10:35:54 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,35 +1,35 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 const double PI = 3.14;  // Задаем значение PI вручную
 
 // Функция для вычисления площади круга
-double calculateCircleArea(double radius) {
+double Area(double radius) {
     return PI * radius * radius;
 }
 
 // Функция для вычисления площади кольца
 double calculateRingArea(double outerRadius, double innerRadius) {
-    double outerCircleArea = calculateCircleArea(outerRadius);
-    double innerCircleArea = calculateCircleArea(innerRadius);
+    double vneshniRadius = Area(vneshniRadius);
+    double vnutriRadius = Area(vnutriRadius);
 
     return outerCircleArea - innerCircleArea;
 }
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    double outerRadius, innerRadius;
+    double vneshniRadius, vnutriRadius;
     std::cout << "Введите внешний радиус кольца: ";
-    std::cin >> outerRadius;
+    std::cin >> vneshniRadius;
     std::cout << "Введите внутренний радиус кольца: ";
-    std::cin >> innerRadius;
+    std::cin >> vnutriRadius;
 
-    if (outerRadius <= innerRadius) {
+    if (vneshniRadius <= vnutriRadius) {
         std::cerr << "Ошибка: Внешний радиус должен быть больше внутреннего." << std::endl;
         return 1; // Возврат кода ошибки
     }
 
-    double ringArea = calculateRingArea(outerRadius, innerRadius);
+    double ringArea = Area(vneshniRadius, vnutriRadius);
     std::cout << "Площадь кольца: " << ringArea << std::endl;
 
     return 0;
