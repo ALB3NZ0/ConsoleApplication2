cf7049d5cd579d19e9b6327b201b06e3f35a3527
Rustam <144844263+ALB3NZ0@users.noreply.github.com>
2024-02-07 10:50:37 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <cmath>
 
-const double PI = 3.14;  // Задаем значение PI вручную
+const double PI = 3.14;  
 
-// Функция для вычисления площади круга
-double Area(double radius) {
+// вычисления площади круга
+int Area(int radius) {
     return PI * radius * radius;
 }
 
-// Функция для вычисления площади кольца
-double calculateArea(double vneshniRadius, double vnutriRadius) {
-    double vneshniArea = Area(vneshniRadius);
-    double vnutriArea = Area(vnutriRadius);
+//  вычисления площади кольца
+int calculateArea(int vneshniRadius, int vnutriRadius) {
+    int vneshniArea = Area(vneshniRadius);
+    int vnutriArea = Area(vnutriRadius);
 
     return vneshniArea - vnutriArea;
 }
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    double vneshniRadius, vnutriRadius;
+    int vneshniRadius, vnutriRadius;
     std::cout << "Введите внешний радиус кольца: ";
     std::cin >> vneshniRadius;
     std::cout << "Введите внутренний радиус кольца: ";
     std::cin >> vnutriRadius;
 
-    double ringArea = calculateArea(vneshniRadius, vnutriRadius);
+    int ringArea = calculateArea(vneshniRadius, vnutriRadius);
     std::cout << "Площадь кольца: " << ringArea << std::endl;
 
     return 0;
